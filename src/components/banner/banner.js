c26ddae5c71a86008423016224782c4687aff6fb
Caily_QuocThinh <quocthinh_web@caily.com.vn>
2023-03-06 15:33:18 +0700

    update

---
+++ src/components/banner/banner.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../../assets/css/styles.css';
+
+const Banner = () =>{
+    return(
+        <>
+         <div className="right">sss</div>
+        </>
+
+    )
+}
+
+export default Banner;
