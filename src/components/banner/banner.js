af8568e84879976be5d55bf44e57f6bebed86a1b
Caily_QuocThinh <quocthinh_web@caily.com.vn>
2023-03-08 16:18:34 +0700

    update

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -1,13 +1,21 @@
 import React from "react";
-import '../../assets/css/styles.css';
-
-const Banner = () =>{
-    return(
-        <>
-         <div className="right">sss</div>
-        </>
-
-    )
-}
+import "./banner.scss";
+// import $ from 'jquery';
+import imagesBanner from "../../assets/img/header.png";
+import headerImg from "../../assets/img/header-border-top.png";
+const mystyle = {
+  backgroundSize: "cover",
+  backgroundImage: "url(" + imagesBanner + ")",
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center center",
+};
+const Banner = () => {
+  return (
+    <>
+		
+		
+    </>
+  );
+};
 
 export default Banner;
