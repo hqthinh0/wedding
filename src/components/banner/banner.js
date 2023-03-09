79555bbaa279a0cf034fb854482af5017120552b
Caily_QuocThinh <quocthinh_web@caily.com.vn>
2023-03-09 10:16:46 +0700

    Banner finish

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./banner.scss";
 // import $ from 'jquery';
 import imagesBanner from "../../assets/img/header.png";
-import headerImg from "../../assets/img/header-border-top.png";
+// import headerBorderTop from "../../assets/img/header-border-top.png";
+// import headerBorderBot from "../../assets/img/header-border-bottom.png";
 const mystyle = {
   backgroundSize: "cover",
   backgroundImage: "url(" + imagesBanner + ")",
@@ -12,7 +13,30 @@ const mystyle = {
 const Banner = () => {
   return (
     <>
-		
+		<section>
+        <div className="box-mainvisual">
+          <div className="box-mainvisual__bg" style={mystyle}>
+            <div className="wrp-container wrp-box__content">
+              <div className="box-mainvisual__border"></div>
+              <div className="wrp-container">
+                <div className="box-mainvisual__txt">  
+                  <h2 className="hdg-lv2 lv2 box-mainvisual__txt--bride box-mainvisual__text">võ hoàng thanh hân</h2>
+
+
+                  {/* <div className=" ">
+                   
+                    </div> */}
+                  <div className="box-mainvisual__rings"></div>
+                  <h2 className="hdg-lv2 lv2 box-mainvisual__txt--groom box-mainvisual__text">nguyễn hoàng gia bảo</h2>
+                  {/* <div className="">
+                    
+                  </div> */}
+                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
 		
     </>
   );
